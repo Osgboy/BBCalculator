bffe85123d7e27b34660bca3d2fbac00f807ae4d
Oliver <olivergu100@gmail.com>
2024-08-03 02:37:04 -0400

    asdf

--- BBCalculator/urls.py
+++ BBCalculator/urls.py
@@ -20,4 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('calculator.urls'))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
