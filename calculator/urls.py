b227da73cd3d006b5daf1b15b73a06438f639157
Oliver <olivergu100@gmail.com>
2024-08-13 11:05:51 -0400

    Rolling back progress bar attempt

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,21 +1,6 @@
 from django.urls import path
-from django.http import HttpResponse
-from .models import Progress
-import json
 from . import views
-
-
-def get_progress(request, pID):
-    p = Progress.objects.get(id=pID)
-    progressState = {
-        'progress': p.progress,
-        'max_progress': p.max_progress
-    }
-    print(f'Task: {pID} with progress of {p.progress}/{p.max_progress}')
-    return HttpResponse(json.dumps(progressState), content_type='application/json')
-
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('progress/<int:pID>/', get_progress, name='progress_status')
 ]
