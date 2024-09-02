7fa3b603623702d36dc4a7ede19ba6e13f652ae9
Osgboy <32489148+Osgboy@users.noreply.github.com>
2024-09-02 19:35:34 -0400

    Added icon url redirects

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
+    path('apple-touch-icon.png', RedirectView.as_view(url='/static/apple-touch-icon.png', permanent=True))
 ]
